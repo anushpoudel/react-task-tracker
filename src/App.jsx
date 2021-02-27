99c1ab478c9f5588ac33b23194206d14879e6dbc
Anush Poudel <apoudel55@gmail.com>
2021-02-27 16:29:04 +1100

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,112 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 0,
-      text: "Hello there",
-      day: "Feb 25th",
-    },
-  ]);
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    })();
+  }, []);
+
+  //Fetch tasks
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:4000/tasks");
+    return res.json();
+  };
+
+  //Fetch task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:4000/tasks/${id}`);
+    return res.json();
+  };
+
+  //Add Task
+  const addTask = async (task) => {
+    const res = await fetch(`http://localhost:4000/tasks/`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    const data = await res.json();
+
+    setTasks((prev) => [...prev, data]);
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = { id, ...task };
+    // setTasks([...tasks, newTask]);
+  };
 
   //Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:4000/tasks/${id}`, {
+      method: "DELETE",
+    });
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  //Toggle reminder
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = {
+      ...taskToToggle,
+      reminder: !taskToToggle.reminder,
+    };
+
+    const res = await fetch(`http://localhost:4000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updatedTask),
+    });
+
+    const data = await res.json();
+
+    setTasks(tasks.map((task) => (task.id === id ? { ...data } : task)));
+  };
+
   return (
-    <div className="container">
-      <Header />
-      {tasks.length ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
-      ) : (
-        "No tasks"
-      )}
-    </div>
+    <Router>
+      <div className="container">
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <>
+              {showAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length ? (
+                <Tasks
+                  tasks={tasks}
+                  onDelete={deleteTask}
+                  onToggle={toggleReminder}
+                />
+              ) : (
+                "No tasks"
+              )}
+            </>
+          )}
+        />
+        <Route path="/abouti" component={About} />
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,51 @@
+// import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 const Button = ({ color, text, onClick }) => {
+  // const [count, setCount] = useState(0);
+  // const [apple, setApple] = useState(0);
+
+  // // runs on every render
+  // useEffect(() => {
+  //   console.log("effect");
+  // });
+
+  // // runs on initial render
+  // useEffect(() => {
+  //   console.log("initial effect");
+  // }, []);
+
+  // // runs everytime count is mutated
+  // useEffect(() => {
+  //   console.log("count:", count);
+  // }, [count]);
+
+  // // runs everytime apple is mutated
+  // useEffect(() => {
+  //   console.log("apple:", apple);
+  // }, [apple]);
+
+  // const onClick = () => setCount((prev) => prev + 1);
+
   return (
-    <button
-      onClick={onClick}
-      style={{ backgroundColor: color }}
-      className="btn"
-    >
-      {text}
-    </button>
+    <>
+      <button
+        onClick={onClick}
+        style={{ backgroundColor: color }}
+        className="btn"
+      >
+        {text}
+      </button>
+      {/* <div>{count}</div>
+      <button
+        onClick={() => setApple((prev) => prev + 1)}
+        style={{ backgroundColor: color }}
+        className="btn"
+      >
+        {text} apple
+      </button>
+      <div>{apple}</div> */}
+    </>
   );
 };
 
